41805a9ac5b18b96fa62722f7700ae6ed975a03b
ylzhou00 <yl_zhou@pku.edu.cn>
2019-09-16 11:33:16 +0800

    d

--- src/main/java/com/zyl/algo_dev/others/Test07.java
+++ src/main/java/com/zyl/algo_dev/others/Test07.java
@@ -3,5 +3,6 @@ package com.zyl.algo_dev.others;
 public class Test07 {
     public static void main(String[] args) {
         System.out.println("i am TEST06 modified!!! and modify in github");
+        System.out.println("i am master branch");
     }
 }
