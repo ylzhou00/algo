2de97268636504362668d55b49e9dd9be07a64a0
ylzhou00 <34699326+ylzhou00@users.noreply.github.com>
2019-09-16 11:11:20 +0800

    Update Test05.java

--- src/main/java/com/zyl/algo_dev/others/Test05.java
+++ src/main/java/com/zyl/algo_dev/others/Test05.java
@@ -2,6 +2,6 @@ package com.zyl.algo_dev.others;
 
 public class Test05 {
     public static void main(String[] args) {
-        System.out.println("i am TEST05 modified!!! and modify in local");
+        System.out.println("i am TEST05 modified!!! and modify in github");
     }
 }
