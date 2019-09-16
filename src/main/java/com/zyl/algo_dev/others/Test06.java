fcf63dc0e15eaa72d3f505a62aaa092f39bcdf34
ylzhou00 <yl_zhou@pku.edu.cn>
2019-09-16 11:15:52 +0800

    b

--- src/main/java/com/zyl/algo_dev/others/Test06.java
+++ src/main/java/com/zyl/algo_dev/others/Test06.java
@@ -2,6 +2,6 @@ package com.zyl.algo_dev.others;
 
 public class Test06 {
     public static void main(String[] args) {
-        System.out.println("i am TEST06 modified!!! and modify in github");
+        System.out.println("i am TEST06 modified!!! and modify in local");
     }
 }
